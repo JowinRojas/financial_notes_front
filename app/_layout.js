923867b3301b553cb4ced3528f234a8244758cc9
jowin rojas <jowinx-001@hotmail.com>
2024-08-22 15:09:17 -0500

    creacion de proyecto

---
+++ app/_layout.js
@@ -0,0 +1,10 @@
+import { Stack } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Layout() {
+  return (
+    <View>
+      <Stack></Stack>
+    </View>
+  );
+}
---
+++ app/index.js
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text>Welcome to the financial notes app!</Text>
+      <Text>This is the main screen.</Text>
+    </View>
+  );
+}
